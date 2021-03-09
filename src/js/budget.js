1234b4b304c1fda3fe7c6ecf821a28c7cd0da0d2
Manocchio <guruu.leste@gmail.com>
2021-03-09 18:59:40 -0300

    arrumando simulador financeiro

--- src/js/budget.js
+++ src/js/budget.js
@@ -4,16 +4,22 @@ function calculateBudget() {
 
   // PRICES R$
   const priceArduino = 50
-  const priceSensor = 20
-  const priceRepeater = 300
-
+  const priceSensor = 20.50
+  const priceRepeater = 300.79
   const areaDelimitation = 500
-  const areas = Math.round(meters2 / areaDelimitation) <= 500 ? 1 : Math.round(meters2 / areaDelimitation);
-  const numSensors = 4
-  const stonks = 0.3
 
-  const priceEquipment = areas * (priceArduino + priceSensor) * numSensors + (areas * priceRepeater)
-  const total = priceEquipment + priceEquipment * stonks
 
-  document.getElementById('total').value = `Total: R$${total}`
+  let preco = (Math.round((meters2 / areaDelimitation) * 4) * (priceSensor + priceArduino)) + ((Math.round((meters2 / areaDelimitation)) * priceRepeater));
+
+  let total = preco + (preco * 0.3);
+
+  // const areas = meters2 / areaDelimitation <= 500 ? 1 : meters2 / areaDelimitation;
+  // const numSensors = 4
+  // const stonks = 0.3
+
+  // const priceEquipment = areas * (priceArduino + priceSensor) * numSensors + (areas * priceRepeater)
+  // let total = priceEquipment + priceEquipment * stonks
+
+
+  document.getElementById('total').value = `Total: R$ ${total.toFixed(2).replace(".", ",")}`
 }
