98308d85b99330f231f47ba5e3627c1226287625
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2021-03-09 00:43:55 -0300

    calculo do simulador financeiro

---
+++ src/js/budget.js
@@ -0,0 +1,22 @@
+
+document.getElementById('btn-calculate')
+  .addEventListener('click', calculateBudget)
+
+function calculateBudget() {
+  const meters2 = document.getElementById('meters2').value
+  
+  // PRICES R$
+  const priceArduino = 50
+  const priceSensor = 20
+  const priceRepeater = 300
+
+  const areaDelimitation = 500
+  const areas = Math.trunc(meters2 / areaDelimitation)
+  const numSensors = 4
+  const stonks = 0.3
+
+  const priceEquipment = areas * (priceArduino + priceSensor) * numSensors + (areas * priceRepeater)
+  const total = priceEquipment + priceEquipment * stonks
+
+  document.getElementById('total').value = total
+}
